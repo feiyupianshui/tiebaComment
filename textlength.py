196fcfaa6b8130a0ece855082fefb8e340c7d8fd
feiyupianshui <ed273030@gmail.com>
2017-11-16 18:23:33 +0800

    20171116

--- textlength.py
+++ textlength.py
@@ -4,4 +4,9 @@ data = open("C:\\Users\\D18\\Desktop\\contragambling\\nogamblingcomments.txt", '
 x = data.read()
 pattern = re.compile('[A-Za-z0-9_]+|\n+|[-_=+.,"?!“”～，；？。！、…—（）@#\\\\ ]+')
 result = re.sub(pattern, "", x)
-print(len(x))
+words = [x for x in jieba.cut(result) if len(x) >= 2]
+c = Counter(words).most_common(20)
+with open('TFrequency.csv', 'w+', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(c)
+print(c, end=' ')
